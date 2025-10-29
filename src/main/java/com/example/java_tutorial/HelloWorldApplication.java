7950c4fd9fe328e58b8d64d623203785decf2188
Gvantsa Kevkhishvili <gvantsa.kevkhishvili@leverx.com>
2025-10-29 15:21:08 +0400

    first app

---
+++ src/main/java/com/example/java_tutorial/HelloWorldApplication.java
@@ -0,0 +1,18 @@
+package com.example.java_tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+
+        logger.info("Hello World!");
+    }
+
+}
